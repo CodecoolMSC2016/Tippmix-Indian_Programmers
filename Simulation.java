790c91b068ee08f77f93626f815655bed37424c6
imre_meszesan <imre.meszesan@gmail.com>
2017-02-01 14:46:57 +0100

    added Simulation constructor

--- Simulation.java
+++ Simulation.java
@@ -1,15 +1,32 @@
 import java.io.*;
+import java.util.Random;
+import java.util.ArrayList;
+
 
 public class Simulation {
-	private String[] data;
-	public Simulation() {
-		/**this.home_team;
-		this.guest_team;
-		this.home_score;
-		this.guest_score;
-		this.odds;
-		this.data = load();		
-		//Stores the data in instance variable.**/
+	static String[] data;
+	public ArrayList<ArrayList<String>> teamPairs;
+	public float odds;
+	public Simulation(int rounds) {
+		teamPairs = new ArrayList<ArrayList<String>>();
+		Random teamnumber = new Random();
+		Random score= new Random();
+		ArrayList<String> teams = new ArrayList<String>();
+		load();
+		for(int i = 0; i < rounds; i++) {
+		String team1=data[teamnumber.nextInt(data.length -1)];
+		String force1=team1.split("\t")[1];
+		team1 = team1.split("\t")[0];
+		String team2=data[teamnumber.nextInt(data.length -1)];
+		String force2=team2.split("\t")[1];
+		team2 = team2.split("\t")[0];
+		double odds = Integer.parseInt(force1)/Integer.parseInt(force2);
+
+			teams.add(team1 + ": " + team2 + ":"+ score.nextInt(10) + ":"+ score.nextInt(10)+ ":" + odds);
+			teamPairs.add(teams);
+			teams = new ArrayList<String>();
+		}
+	System.out.println(teamPairs);
 	} 
 
 	public static void load() {
@@ -20,12 +37,13 @@ public class Simulation {
 			while (c != -1){
 				 c = in.read();
 				text += (char)c;
-
 			}
-			System.out.println(text.split("\t"));
-		}catch(FileNotFoundException e){
-		}catch(IOException e){}
-		//Loads from CSV file and return the result.
+			String[] splitText = text.split("\n");
+			data = new String[splitText.length];
+			data = splitText;
+		} catch(FileNotFoundException e) {
+		} catch(IOException e) {}
+
 	}
 	public void generateData() {
 		//Write simulated round to CSV file.
