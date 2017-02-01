d4f5c90b208419f30d16e5561d9036f612c88d1a
imre_meszesan <imre.meszesan@gmail.com>
2017-02-01 14:47:56 +0100

    created constructor with 1 paramater

--- Simulator.java
+++ Simulator.java
@@ -1,3 +1,14 @@
-public class Simulator{
-	Simulator
+public class Simulator {
+	public ArrayList<ArrayList<String>> teamPairs;
+	public float odds;
+	
+	Simulator(Simulation simulation){ // add Logger paramater
+		this.teamPairs = simulation.teamPairs;
+		
+	}
+		
+	public Result run(){
+		
+		
+	}
 }
