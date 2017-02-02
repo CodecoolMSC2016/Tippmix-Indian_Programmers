6896f0fadfe942896cdc55321c5357ee27618fbc
imre_meszesan <imre.meszesan@gmail.com>
2017-02-02 10:17:30 +0100

    added test print

--- Simulator.java
+++ Simulator.java
@@ -1,14 +1,28 @@
+import java.util.ArrayList;
+
 public class Simulator {
 	public ArrayList<ArrayList<String>> teamPairs;
+	public Logger log;
 	public float odds;
 	
-	Simulator(Simulation simulation){ // add Logger paramater
+	Simulator(Simulation simulation, Logger logger){ // add Logger paramater
 		this.teamPairs = simulation.teamPairs;
+		this.log = logger;
+		
 		
 	}
 		
-	public Result run(){
+	public void run(){
+		//for (ArrayList team: teamPairs){			
+		//	log.print(team.get(0).toString());
+		//	log.print(team.get(1).toString());
+		//}
+		Result result = new Result();
+		result.addTeam(teamPairs.get(0));
 		
+		
+		 
+			
 		
 	}
 }
