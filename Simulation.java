d659919ca7977bb4108586c970fdb628814c9148
imre_meszesan <imre.meszesan@gmail.com>
2017-02-02 10:16:23 +0100

    added tabs

--- Simulation.java
+++ Simulation.java
@@ -14,19 +14,18 @@ public class Simulation {
 		ArrayList<String> teams = new ArrayList<String>();
 		load();
 		for(int i = 0; i < rounds; i++) {
-		String team1=data[teamnumber.nextInt(data.length -1)];
-		String force1=team1.split("\t")[1];
-		team1 = team1.split("\t")[0];
-		String team2=data[teamnumber.nextInt(data.length -1)];
-		String force2=team2.split("\t")[1];
-		team2 = team2.split("\t")[0];
-		double odds = Integer.parseInt(force1)/Integer.parseInt(force2);
-
+			String team1=data[teamnumber.nextInt(data.length -1)];
+			String force1=team1.split("\t")[1];
+			team1 = team1.split("\t")[0];
+			String team2=data[teamnumber.nextInt(data.length -1)];
+			String force2=team2.split("\t")[1];
+			team2 = team2.split("\t")[0];
+			double odds = Integer.parseInt(force1)/Integer.parseInt(force2);
 			teams.add(team1 + ": " + team2 + ":"+ score.nextInt(10) + ":"+ score.nextInt(10)+ ":" + odds);
 			teamPairs.add(teams);
 			teams = new ArrayList<String>();
 		}
-	System.out.println(teamPairs);
+		System.out.println(teamPairs);
 	} 
 
 	public static void load() {
