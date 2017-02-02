4a2af60ffaa3b9061294463f5c7a9ef6b36e9d81
imre_meszesan <imre.meszesan@gmail.com>
2017-02-02 10:14:39 +0100

    added overloaded print method

--- Logger.java
+++ Logger.java
@@ -1,1 +1,13 @@
-public class Logger{}
+public class Logger{
+	
+	public void print(String str, boolean newLine){
+		if (newLine){
+			System.out.println(str);
+		}else {
+			System.out.print(str);
+		}
+	}
+	public void print(String str){
+		System.out.print(str);
+	}
+}
