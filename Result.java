2ddc43caa6a90fd9a5810346c8c60f984f791960
imre_meszesan <imre.meszesan@gmail.com>
2017-02-02 10:16:03 +0100

    added HashMap

--- Result.java
+++ Result.java
@@ -1,1 +1,12 @@
-public class Result extends Simulator {}
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class Result {
+	private HashMap<String, Integer> results;
+
+
+	public void addTeam(ArrayList<String> match){
+		System.out.print(match);
+	}
+
+}
